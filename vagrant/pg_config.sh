ac4d359e945a5864f1652743926fada7d20ad1b7
brenj <brenj@mailbox.org>
2015-11-28 12:38:58 -0800

    Add required packages to pg_config
    This commit:
    
        * Adds dependencies for using psycopg2 >= 2.5
        * Adds virtualenv

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -1,7 +1,10 @@
 apt-get -qqy update
 apt-get -qqy install git
-apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install postgresql
+apt-get -qqy install libpq-dev python-dev
 apt-get -qqy install python-pip
+
+pip install virtualenv
 
 git clone https://github.com/brenj/udacity.git
 mv ./udacity /vagrant
