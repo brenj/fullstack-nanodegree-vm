f24bb45ee40911cc822d4550daa908eb5178157e
brenj <brenj@mailbox.org>
2018-01-21 09:41:58 -0600

    Update repo to pull down for the project

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -6,8 +6,8 @@ apt-get -qqy install python-pip
 
 pip install virtualenv
 
-git clone https://github.com/brenj/udacity.git
-mv ./udacity /vagrant
+git clone https://github.com/brenj/tech-quote.git
+mv ./tech-quote /vagrant
 
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
